bcf2736e0705493c897fcf68451528763f2f5d9e
Kyle Pearson <k.pearson112@gmail.com>
2025-02-09 12:56:59 -0800

    new upscale workflow + env

--- predict.py
+++ predict.py
@@ -1,5 +1,8 @@
 import os
 import json
+import shutil
+import tarfile
+import zipfile
 from typing import List
 
 # uncomment to run cog predict
@@ -7,7 +10,7 @@ from typing import List
 #load_dotenv()
 
 from PIL import Image
-from cog import BasePredictor, Input
+from cog import BasePredictor, Input, Path
 from comfyui import ComfyUI
 
 from minio_manager import MinioStorageManager as CloudStorageManager
@@ -19,7 +22,8 @@ ALL_DIRECTORIES = [OUTPUT_DIR, INPUT_DIR, COMFYUI_TEMP_OUTPUT_DIR]
 
 WORKFLOWS = {
     'base': os.environ.get('WORKFLOW_IMAGE', 'workflows/360-panorama-sdxl-depth.json'),
-    'upscale': os.environ.get('WORKFLOW_IMAGE_UPSCALE', 'workflows/360-panorama-sdxl-upscale.json')
+    'upscale': os.environ.get('WORKFLOW_IMAGE_UPSCALE', 'workflows/360-panorama-sdxl-upscale-inpaint-depth.json'), # from prompt
+    'upscale-input': os.environ.get('WORKFLOW_IMAGE_UPSCALE_INPUT', 'workflows/360-panorama-sdxl-input-upscale-inpaint-depth.json') # from id
 }
 
 BUCKETS = {
@@ -46,11 +50,63 @@ class Predictor(BasePredictor):
         except Exception as e:
             raise(f"Failed to connect to Minio: {e}\ntry adjusting environment variables")
 
+
+    def handle_input_file(self, input_file: Path):
+        file_extension = self.get_file_extension(input_file)
+
+        if file_extension == ".tar":
+            with tarfile.open(input_file, "r") as tar:
+                tar.extractall(INPUT_DIR)
+        elif file_extension == ".zip":
+            with zipfile.ZipFile(input_file, "r") as zip_ref:
+                zip_ref.extractall(INPUT_DIR)
+        elif file_extension in [".jpg", ".jpeg", ".png", ".webp"]:
+            shutil.copy(input_file, os.path.join(INPUT_DIR, f"input{file_extension}"))
+        else:
+            raise ValueError(f"Unsupported file type: {file_extension}")
+
+        print("====================================")
+        print(f"Inputs uploaded to {INPUT_DIR}:")
+        self.comfyUI.get_files(INPUT_DIR)
+        print("====================================")
+
+    def get_file_extension(self, input_file: Path) -> str:
+        file_extension = os.path.splitext(input_file)[1].lower()
+        if not file_extension:
+            with open(input_file, "rb") as f:
+                file_signature = f.read(4)
+            if file_signature.startswith(b"\x1f\x8b"):  # gzip signature
+                file_extension = ".tar"
+            elif file_signature.startswith(b"PK"):  # zip signature
+                file_extension = ".zip"
+            else:
+                try:
+                    with Image.open(input_file) as img:
+                        file_extension = f".{img.format.lower()}"
+                        print(f"Determined file type: {file_extension}")
+                except Exception as e:
+                    raise ValueError(
+                        f"Unable to determine file type for: {input_file}, {e}"
+                    )
+        return file_extension
+
     def predict(
         self,
+        input_file: Path = Input(
+            description="Input image, tar or zip file. Read guidance on workflows and input files here: https://github.com/fofr/cog-comfyui. Alternatively, you can replace inputs with URLs in your JSON workflow and the model will download them.",
+            default=None,
+        ),
+        input_file_id: str = Input(
+            description="Input image to download from cloud storage",
+            default=None,
+        ),
+        bucket: str = Input(
+            description="Bucket to upscale image from on cloud storage",
+            default="360-panorama-sdxl"
+        ),
         prompt: str = Input(
             description="Describe the image",
-            default="Magical pizza kingdom"            
+            default="Glowing mushrooms around pyramids amidst a cosmic backdrop"            
         ),
         suffix_prompt: str = Input(
             description="Suffix prompt for image generation (e.g., '360 panorama, equirectangular')",
@@ -60,49 +116,13 @@ class Predictor(BasePredictor):
             description="Negative prompt for image generation (e.g., 'boring, text, signature, watermark, low quality, bad quality, grainy, blurry, long neck, closed eyes')",
             default="boring, text, signature, watermark, low quality, bad quality, grainy, blurry"
         ),
-        width: int = Input(
-            description="Width of the image",
-            default=2048
-        ),
-        height: int = Input(
-            description="Height of the image",
-            default=1024
-        ),
-        seed: int = Input(
-            description="Seed for the model, if negative will be random",
-            default=-1
-        ),
         lora_strength: float = Input(
             description="Strength of the LoRA model",
             default=1.0
         ),
-        cfg: float = Input(
-            description="CFG value for the model",
-            default=3.50
-        ),
-        steps: int = Input(
-            description="Number of steps for the model",
-            default=12
-        ),
-        sampler: str = Input( 
-            description="Sampler for the model",
-            default="dpmpp_sde"
-        ),
-        scheduler: str = Input(
-            description="Scheduler for the model",
-            default="ddim_uniform"
-        ),
-        tile_x: bool = Input(
-            description="Tile the image in the x direction",
-            default=True
-        ),
-        tile_y: bool = Input(
-            description="Tile the image in the y direction",
-            default=False
-        ),
         upscale_by: float = Input(
             description="Upscale the image by this factor",
-            default=0.0
+            default=2.0
         ),
         upscale_steps: int = Input(
             description="Number of steps for upscaling",
@@ -129,52 +149,72 @@ class Predictor(BasePredictor):
             choices=["webp", "jpg", "png"],
             default="webp",
         )
-
-    ) -> List[str]: # for local deployment
+        
+        ) -> List[str]:
 
         # clean up directories
         self.comfyUI.cleanup(ALL_DIRECTORIES)
 
         # load the workflow
-        if upscale_by > 1:
-            self.cloud.bucket = BUCKETS['upscale']
-            with open(WORKFLOWS['upscale'], "r") as file:
-                EXAMPLE_WORKFLOW_JSON = json.loads(file.read())
+        if input_file:
+            EXAMPLE_WORKFLOW_JSON = WORKFLOWS['upscale-input']
+        elif input_file_id:
+            EXAMPLE_WORKFLOW_JSON = WORKFLOWS['upscale-input']
         else:
-            self.cloud.bucket = BUCKETS['base']
-            with open(WORKFLOWS['base'], "r") as file:
-                EXAMPLE_WORKFLOW_JSON = json.loads(file.read())
+            if upscale_by > 1:
+                EXAMPLE_WORKFLOW_JSON = WORKFLOWS['upscale']
+            else:
+                EXAMPLE_WORKFLOW_JSON = WORKFLOWS['base']
 
         wf = self.comfyUI.load_workflow(EXAMPLE_WORKFLOW_JSON)
 
+        # handle input file
+        if input_file:
+            self.handle_input_file(input_file)
+
+            # Update the input file in the workflow JSON
+            wf['130']['inputs']['image'] = os.path.join(INPUT_DIR, f"input{os.path.splitext(input_file)[1]}")
+
+            # build the prompts
+            wf['6']['inputs']['text'] = prompt
+            wf['6']['inputs']['text'] += ", " + suffix_prompt
+            wf['7']['inputs']['text'] = negative_prompt
+
+        elif input_file_id:
+            # build the path to image.webp
+            cloud_path = f"{input_file_id.strip('/')}/image.webp"
+
+            # download the input file from cloud storage
+            input_file = self.cloud.download_file_to_disk(cloud_path, f"{INPUT_DIR}/input.webp", bucket)
+
+            # Update the input file in the workflow JSON
+            wf['130']['inputs']['image'] = os.path.join(INPUT_DIR, f"input.webp")
+
+            # download + parse workflow
+            cloud_path = f"{input_file_id.strip('/')}/workflow.json"
+            self.cloud.download_file_to_disk(cloud_path, f"{INPUT_DIR}/workflow.json", bucket)
+
+            with open(f"{INPUT_DIR}/workflow.json", "r") as file:
+                wf_og = json.loads(file.read())
+            
+            # update the workflow
+            wf['6']['inputs']['text'] = wf_og['6']['inputs']['text'] # prompt
+            wf['7']['inputs']['text'] = wf_og['7']['inputs']['text'] # negative prompt
+        else:
+            # build the prompts
+            wf['6']['inputs']['text'] = prompt
+            wf['6']['inputs']['text'] += ", " + suffix_prompt
+            wf['7']['inputs']['text'] = negative_prompt
+
+    
         # connect to comfyUI
         self.comfyUI.connect()
-
-        # update the workflow with the inputs
-        wf['5']['inputs']['width'] = width
-        wf['5']['inputs']['height'] = height
-
-        # build the prompts
-        wf['6']['inputs']['text'] = prompt
-        wf['6']['inputs']['text'] += ", " + suffix_prompt
-        wf['7']['inputs']['text'] = negative_prompt
 
         # lora config
         wf['10']['inputs']['strength_model'] = lora_strength
 
         # seed
         self.comfyUI.randomise_seeds(wf)
-
-        if seed > 0:
-            wf['11']['inputs']['seed'] = seed
-
-        # sampler
-        wf['11']['inputs']['cfg'] = cfg
-        wf['11']['inputs']['steps'] = steps
-        wf['11']['inputs']['tileX'] = int(tile_x)
-        wf['11']['inputs']['tileY'] = int(tile_y)
-        wf['11']['inputs']['sampler_name'] = sampler
-        wf['11']['inputs']['scheduler'] = scheduler
 
         # upscaling
         if upscale_by > 1:
@@ -208,6 +248,8 @@ class Predictor(BasePredictor):
 
         # back up images on cloud storage
         image_hash = self.cloud.hash_file(saved_images[1])
+        if input_file_id:
+            image_hash = input_file_id.strip('/')
 
         # upload image
         try:
@@ -235,7 +277,7 @@ class Predictor(BasePredictor):
 
         # create thumbnails
         image_thumbnail = self.cloud.resize_image(saved_images[1])
-    
+        
         # Save the resized image
         Image.fromarray(image_thumbnail).save('image_thumbnail.webp', format='WEBP')
 
@@ -259,7 +301,7 @@ class Predictor(BasePredictor):
         try:
             depth_thumbnail_url = self.cloud.upload_file('depth_thumbnail.webp', f"{image_hash}/depth_thumbnail.webp", 'image/webp')
             print(f"Depth thumbnail uploaded to: {depth_thumbnail_url}")
-        except Exception as e:
+        except Exception as e:            
             print(f"Failed to upload depth thumbnail: {e}")
 
             if 'duplicate' in str(e).lower():
@@ -288,16 +330,7 @@ class Predictor(BasePredictor):
             "prompt": prompt,
             "suffix_prompt": suffix_prompt,
             "negative_prompt": negative_prompt,
-            "width": width,
-            "height": height,
-            "seed": wf['11']['inputs']['seed'],
             "lora_strength": lora_strength,
-            "cfg": cfg,
-            "steps": steps,
-            "tile_x": tile_x,
-            "tile_y": tile_y,
-            "sampler": sampler,
-            "scheduler": scheduler,
             "upscale_by": upscale_by,
             "upscale_steps": upscale_steps,
             "upscale_sampler": upscale_sampler,
@@ -325,5 +358,7 @@ class Predictor(BasePredictor):
                 metadata_url = self.cloud.get_file_url(f"{image_hash}/metadata.json")
             else:
                 metadata_url = None
+
+        # TODO create animations if upscale_by > 1
 
         return [depth_url, image_url, metadata_url]
